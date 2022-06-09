4ee5bc2509551ef368ca511b95a10ee79e489f98
akrem <akremstudy@gmail.com>
2022-06-09 09:28:14 -0400

    getter function test

--- test/functionTestsKeeper-TellorAutopay.js
+++ test/functionTestsKeeper-TellorAutopay.js
@@ -10,15 +10,15 @@ describe("KEEPER - function tests", () => {
     let tellor,autopay,accounts,token,blocky;
     let abiCoder = new ethers.utils.AbiCoder();
 
-    const MAXGASCOVER = BigNumber.from((web3.utils.toWei("1")));
-    const TIP = web3.utils.toWei("99");
-    const FUNCTIONSIG = "0x57806e707c9ca4cc348680e2d4637472fc51228a079cb8a6a8cba51fe6f4ebbb3a930c8db9d5e25dabd5f0a48f45f5b6b524bac100df05eaf5311f3e5339ac7c3dd0a37e0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000627abef8"
-    const TIMESTAMP = Math.floor(Date.now()/1000);
-    const TYPE = "TellorKpr"
-    const ARGS = abiCoder.encode(["bytes", "address", "uint256", "uint256", "uint256"],[FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER]);
-    const QUERYDATA = abiCoder.encode(["string","bytes"], [TYPE,ARGS])
-    const KPRQUERYID = keccak256(QUERYDATA);
-    const JOBID = keccak256(abiCoder.encode(["bytes","address","uint256","uint256","uint256","uint256","uint256","uint256"],[FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,400,600,TIP]));
+    let MAXGASCOVER = BigNumber.from((web3.utils.toWei("1")));
+    let TIP = web3.utils.toWei("99");
+    let FUNCTIONSIG = "0x57806e707c9ca4cc348680e2d4637472fc51228a079cb8a6a8cba51fe6f4ebbb3a930c8db9d5e25dabd5f0a48f45f5b6b524bac100df05eaf5311f3e5339ac7c3dd0a37e0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000627abef8"
+    let TIMESTAMP = Math.floor(Date.now()/1000);
+    let TYPE = "TellorKpr"
+    let ARGS = abiCoder.encode(["bytes", "address", "uint256", "uint256", "uint256"],[FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER]);
+    let QUERYDATA = abiCoder.encode(["string","bytes"], [TYPE,ARGS])
+    let KPRQUERYID = keccak256(QUERYDATA);
+    let JOBID = keccak256(abiCoder.encode(["bytes","address","uint256","uint256","uint256","uint256","uint256","uint256"],[FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,400,600,TIP]));
 
 
     beforeEach(async () => {
@@ -34,62 +34,10 @@ describe("KEEPER - function tests", () => {
         autopay = await Autopay.deploy(tellor.address, token.address, accounts[0].address, 10);
         await autopay.deployed();
         await token.approve(autopay.address, h.toWei("1000"));
-        blocky = await h.getBlock();
         await autopay.tipKeeperJob(FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,TIP);
+        blocky = await h.getBlock();
         await autopay.initKeeperJob(FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,400,600,TIP);
         await autopay.fundJob(JOBID,TIP);
-    });
-
-    it("tipKeeperJob", async () => {
-        let autopayBalance = await token.balanceOf(autopay.address);
-        let acctBeforeBalance = await token.balanceOf(accounts[0].address);
-        await autopay.tipKeeperJob(FUNCTIONSIG,accounts[1].address,80001,TIMESTAMP,MAXGASCOVER,TIP);
-        let acctBalDiff = MAXGASCOVER.add(TIP);
-        expect(await token.balanceOf(accounts[0].address)).to.equal(acctBeforeBalance.sub(acctBalDiff));
-        expect(await token.balanceOf(autopay.address)).to.equal(autopayBalance.add(acctBalDiff));
-        });
-    
-    it("increaseMaxGasForExistingJob", async () => {
-        let autopayBefore = await token.balanceOf(autopay.address);
-        await autopay.increaseMaxGasForExistingJob(KPRQUERYID, MAXGASCOVER);
-        expect(await token.balanceOf(autopay.address)).to.equal(autopayBefore.add(MAXGASCOVER));
-        await h.expectThrowMessage(autopay.connect(accounts[1]).increaseMaxGasForExistingJob(KPRQUERYID, MAXGASCOVER)); // Not job creator
-    });
-
-    // it("unclaimedSingleTipsFallback", async () => {
-    //     await h.expectThrowMessage(autopay.unclaimedSingleTipsFallback(KPRQUERYID));
-    //     h.advanceTime(7258000);
-    //     let autopayBefore = await token.balanceOf(autopay.address);
-    //     await autopay.unclaimedSingleTipsFallback(KPRQUERYID);
-    //     expect(await token.balanceOf(autopay.address)).to.equal(autopayBefore.sub(MAXGASCOVER.add(web3.utils.toWei("1"))));
-    // });
-
-    it("keeperClaimTip", async () => {
-        let acct0Bal = await token.balanceOf(accounts[0].address); // owner
-        let acct1Bal = await token.balanceOf(accounts[1].address); // keeper
-        let VAL = abiCoder.encode(["bytes32","address","uint256","uint256"],[keccak256("0x"),accounts[1].address,(await h.getBlock()).timestamp,MAXGASCOVER]);
-        await tellor.connect(accounts[1]).submitValue(KPRQUERYID,VAL,0,QUERYDATA);
-        await h.expectThrow(autopay.keeperClaimTip(KPRQUERYID)); // 12 hour buffer
-        h.advanceTime(43200)
-        await autopay.keeperClaimTip(KPRQUERYID);
-        assert(BigInt(await token.balanceOf(accounts[0].address)) > acct0Bal);
-        assert(BigInt(await token.balanceOf(accounts[1].address)) > acct1Bal);
-    });
-
-    it("initKeeperJob", async () => {
-        let TIP = web3.utils.toWei("2");
-        let JOBID = keccak256(abiCoder.encode(["bytes","address","uint256","uint256","uint256","uint256","uint256","uint256"],[FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,40,80,TIP]));
-        await expect(autopay.initKeeperJob(FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,40,80,TIP)).to.emit(autopay, "NewKeeperJob").withArgs(accounts[0].address,JOBID,QUERYDATA,TIP);
-        let result = await h.expectThrowMessage(autopay.initKeeperJob(FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,40,80,TIP));
-        assert.include(result.message, "job id already exists, fund Job");
-    });
-
-    it("fundJob", async () => {
-        let contractBal = await token.balanceOf(autopay.address);
-        let BAL = await token.balanceOf(accounts[0].address);
-        await expect(autopay.fundJob(JOBID,TIP)).to.emit(autopay, "KeeperJobFunded").withArgs(accounts[0].address,TIP,JOBID);
-        expect(await token.balanceOf(accounts[0].address)).to.equal(BAL.sub(MAXGASCOVER.add(TIP)));
-        expect(await token.balanceOf(autopay.address)).to.equal(contractBal.add(MAXGASCOVER.add(TIP)));
     });
 
     it("claimJobTips", async () => {
@@ -109,5 +57,70 @@ describe("KEEPER - function tests", () => {
         expect(await token.balanceOf(autopay.owner())).to.equal(ownerBal.add((MAXGASCOVER.add(TIP).mul(10)).div(1000)));// owner collects 1 percent fee
         let paid = await h.expectThrowMessage(autopay.claimJobTips(JOBID,TIMESTAMP));
         assert.include(paid.message, "Already paid!");
-    })
+    });
+
+    it("fundJob", async () => {
+        let contractBal = await token.balanceOf(autopay.address);
+        let BAL = await token.balanceOf(accounts[0].address);
+        await expect(autopay.fundJob(JOBID,TIP)).to.emit(autopay, "KeeperJobFunded").withArgs(accounts[0].address,TIP,JOBID);
+        expect(await token.balanceOf(accounts[0].address)).to.equal(BAL.sub(MAXGASCOVER.add(TIP)));
+        expect(await token.balanceOf(autopay.address)).to.equal(contractBal.add(MAXGASCOVER.add(TIP)));
+    });
+
+    it("increaseMaxGasForExistingJob", async () => {
+        let autopayBefore = await token.balanceOf(autopay.address);
+        await autopay.increaseMaxGasForExistingJob(KPRQUERYID, MAXGASCOVER);
+        expect(await token.balanceOf(autopay.address)).to.equal(autopayBefore.add(MAXGASCOVER));
+        await h.expectThrowMessage(autopay.connect(accounts[1]).increaseMaxGasForExistingJob(KPRQUERYID, MAXGASCOVER)); // Not job creator
+    });
+
+    it("initKeeperJob", async () => {
+        let TIP = web3.utils.toWei("2");
+        let JOBID = keccak256(abiCoder.encode(["bytes","address","uint256","uint256","uint256","uint256","uint256","uint256"],[FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,40,80,TIP]));
+        await expect(autopay.initKeeperJob(FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,40,80,TIP)).to.emit(autopay, "NewKeeperJob").withArgs(accounts[0].address,JOBID,QUERYDATA,TIP);
+        let result = await h.expectThrowMessage(autopay.initKeeperJob(FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER,40,80,TIP));
+        assert.include(result.message, "job id already exists, fund Job");
+    });
+
+    it("keeperClaimTip", async () => {
+        let acct0Bal = await token.balanceOf(accounts[0].address); // owner
+        let acct1Bal = await token.balanceOf(accounts[1].address); // keeper
+        let VAL = abiCoder.encode(["bytes32","address","uint256","uint256"],[keccak256("0x"),accounts[1].address,(await h.getBlock()).timestamp,MAXGASCOVER]);
+        await tellor.connect(accounts[1]).submitValue(KPRQUERYID,VAL,0,QUERYDATA);
+        await h.expectThrow(autopay.keeperClaimTip(KPRQUERYID)); // 12 hour buffer
+        h.advanceTime(43200)
+        await autopay.keeperClaimTip(KPRQUERYID);
+        assert(BigInt(await token.balanceOf(accounts[0].address)) > acct0Bal);
+        assert(BigInt(await token.balanceOf(accounts[1].address)) > acct1Bal);
+    });
+
+    it("singleJobBalancebyId", async () => {
+        let args = abiCoder.encode(["bytes", "address", "uint256", "uint256", "uint256"],[FUNCTIONSIG,h.zeroAddress,80001,TIMESTAMP,MAXGASCOVER])
+        let QUERYDATA = abiCoder.encode(["string","bytes"],[TYPE,args]);
+        let QUERYID = keccak256(QUERYDATA);
+        let response = await autopay.singleJobBalancebyId(QUERYID);
+        expect(response.amount).to.equal(TIP);
+        expect(response.timestamp).to.equal(blocky.timestamp);
+        expect(response.timeToCallIt).to.equal(TIMESTAMP);
+        expect(response.maxGasRefund).to.equal(MAXGASCOVER);
+        expect(response.creator).to.equal(accounts[0].address);
+    });
+
+    it("tipKeeperJob", async () => {
+        let autopayBalance = await token.balanceOf(autopay.address);
+        let acctBeforeBalance = await token.balanceOf(accounts[0].address);
+        await autopay.tipKeeperJob(FUNCTIONSIG,accounts[1].address,80001,TIMESTAMP,MAXGASCOVER,TIP);
+        let acctBalDiff = MAXGASCOVER.add(TIP);
+        expect(await token.balanceOf(accounts[0].address)).to.equal(acctBeforeBalance.sub(acctBalDiff));
+        expect(await token.balanceOf(autopay.address)).to.equal(autopayBalance.add(acctBalDiff));
+    });
+    
+
+    // it("unclaimedSingleTipsFallback", async () => {
+    //     await h.expectThrowMessage(autopay.unclaimedSingleTipsFallback(KPRQUERYID));
+    //     h.advanceTime(7258000);
+    //     let autopayBefore = await token.balanceOf(autopay.address);
+    //     await autopay.unclaimedSingleTipsFallback(KPRQUERYID);
+    //     expect(await token.balanceOf(autopay.address)).to.equal(autopayBefore.sub(MAXGASCOVER.add(web3.utils.toWei("1"))));
+    // });
 });
